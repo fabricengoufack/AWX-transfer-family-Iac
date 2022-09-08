7ef4ea9bf5626e45329442e07e8959f8be46bf79
Fabrice Ngoufack <fabrice.ngoufack@omnicell.com>
2022-09-08 12:26:43 -0400

    test

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ module "vpc" {
   name = "aws-tf-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-east-1b"]
+  azs             = ["us-west-1b"]
   private_subnets = ["10.0.1.0/24"]
   public_subnets  = ["10.0.2.0/24"]
 
